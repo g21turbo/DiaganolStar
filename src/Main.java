a77624064f3f8c7fd897cc8e18333ab1c1d8439c
g21turbo <dg21stew@gmail.com>
2023-11-10 20:58:26 -0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void printSquareStar(int number) {
+
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        }
+    }
+
+}
